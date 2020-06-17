5baba9a670d90e20af57990e758dca9c3b06b4c6
tunv3 <tunguyen3765@gmail.com>
2020-06-17 22:22:40 +0700

    v0.0.8

--- proploader.go
+++ proploader.go
@@ -7,7 +7,7 @@ import (
 	"os/user"
 )
 
-func LoadPropsFromUsrHome(filename string) *properties.Properties {
+func LoadPropsUsrHome(filename string) *properties.Properties {
 	usr, err := user.Current()
 	if err != nil {
 		log.Fatal( err )
@@ -15,7 +15,7 @@ func LoadPropsFromUsrHome(filename string) *properties.Properties {
 	return properties.MustLoadFile(usr.HomeDir + string(os.PathSeparator)+filename, properties.UTF8)
 }
 
-func LoadPrjProps(filename string) *properties.Properties {
+func LoadPropsPrj(filename string) *properties.Properties {
 	prjhome, err := os.Getwd()
 	if err != nil {
 		log.Fatal( err )
