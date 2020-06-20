6e9fc88efdb7540dd5d63ba555a77165c5f1992a
tunv3 <tunguyen3765@gmail.com>
2020-06-20 14:45:06 +0700

    v0.1.2

--- converter.go
+++ converter.go
@@ -68,6 +68,64 @@ func ToString(x interface{}) string {
 	}
 }
 
+func ToJson(x interface{}) string {
+	switch x.(type) {
+	case int:
+		return fmt.Sprintf("%d", x)
+	case int64:
+		return fmt.Sprintf("%d", x)
+	case int32:
+		return fmt.Sprintf("%d", x)
+	case sql.NullTime:
+		y, _ := x.(sql.NullTime)
+		if y.Valid {
+			return "\"" + y.Time.Format(DateTimeLayout) + "\""
+		} else {
+			return "\"null\""
+		}
+	case time.Time:
+		y, _ := x.(time.Time)
+		return "\"" + y.String() +"\""
+	case sql.NullInt32:
+		y, _ := x.(sql.NullInt32)
+		if y.Valid {
+			return fmt.Sprintf("%d", y.Int32)
+		} else {
+			return  "\"null\""
+		}
+	case sql.NullInt64:
+		y, _ := x.(sql.NullInt64)
+		if y.Valid {
+			return fmt.Sprintf("%d", y.Int64)
+		} else {
+			return  "\"null\""
+		}
+	case sql.NullString:
+		y, _ := x.(sql.NullString)
+		if y.Valid {
+			return "\"" + fmt.Sprintf("%s", y.String) + "\""
+		} else {
+			return  "\"null\""
+		}
+	case sql.NullFloat64:
+		y, _ := x.(sql.NullFloat64)
+		if y.Valid {
+			return strconv.FormatFloat(y.Float64, 'f', -1, 64)
+		} else {
+			return  "\"null\""
+		}
+	case string:
+		y, _ := x.(string)
+		return "\"" +  y + "\""
+	case []byte:
+		y, _ := x.([]byte)
+		return string(y)
+	default:
+		return "!!!Unknown-!!!"
+	}
+}
+
+
 func CloseRows(rows *sql.Rows) {
 	err := rows.Close()
 	CheckErr(err)
